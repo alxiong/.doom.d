914986c2c8cd24d0cf56bc3cd017ec64d16c59d1
Alex Xiong <alex.xiong.tech@gmail.com>
2019-11-21 16:55:29 +0800

    update20191122

--- config.el
+++ config.el
@@ -2,37 +2,18 @@
 
 ;; Place your private configuration here
 (setq user-full-name "Alex Luoyuan Xiong"
-      user-mail-address "alex.xiong.tech@gmail.com")
+      user-mail-address "alex.xiong.tech@gmail.com"
+      epa-file-encrypt-to user-mail-address
+      ;; line number disable by default, otherwise slows down loading
+      display-line-numbers-type nil)
 
 ;;; Fonts
 ;;; note: do download/install these fonts to the machine first
 (setq doom-font (font-spec :family "Fira Code" :size 13)
       doom-variable-pitch-font (font-spec :family "Noto Sans" :size 15))
 
-;; better code completion with tabnine(use-package company-tabnine :ensure t)
-(defun add-company-tabnine ()
-  (add-to-list (make-local-variable 'company-backends) 'company-tabnine))
-
-(def-package! company-tabnine
-  :config
-  (setq company-idle-delay 0
-        company-show-numbers t)
-  (add-hook! (c-mode-local-vars
-              c++-mode-local-vars
-              emacs-lisp-mode-local-vars
-              python-mode-local-vars)
-    #'add-company-tabnine))
-
 ;; make minial ranger (i.e. deer) the default dir handler
 (ranger-override-dired-mode t)
-
-;; Use the tab-and-go frontend.
-;; Allows TAB to select and complete at the same time.
-(company-tng-configure-default)
-(setq company-frontends
-      '(company-tng-frontend
-        company-pseudo-tooltip-frontend
-        company-echo-metadata-frontend))
 
 ;; Mac specific setting
 (cond ((string-equal system-type "darwin")
@@ -45,6 +26,57 @@
        (setq ns-function-modifier 'hyper) ; make Fn key do Hyper
        ))
 
+;;; Company autocomplete settings
+(after! company
+  (setq company-minimum-prefix-length 1
+        company-idle-delay 0
+        company-tooltip-limit 10
+        company-show-numbers t
+        company-global-modes '(not comint-mode erc-mode message-mode help-mode gud-mode)
+        ))
+
+;;; lsp
+(after! lsp-ui
+  (setq lsp-ui-sideline-enable nil
+        lsp-enable-file-watchers nil)
+  (map!
+   :map lsp-ui-peek-mode-map
+   "h" #'lsp-ui-peek--select-prev-file
+   "j" #'lsp-ui-peek--select-next
+   "k" #'lsp-ui-peek--select-prev
+   "l" #'lsp-ui-peek--select-next-file))
+
+;;; C/C++ setting
+(after! format
+  (set-formatter!
+    'clang-format
+    '("clang-format"
+      ("-assume-filename=%S" (or (buffer-file-name) ""))
+      "-style=Google"))
+  :modes
+  '((c-mode ".c")
+    (c++-mode ".cpp")
+    (java-mode ".java")
+    (objc-mode ".m")
+    ))
+(after! ccls
+  (setq ccls-initialization-options
+        '(:clang (:extraArgs ["-isystem/Library/Developer/CommandLineTools/usr/include/c++/v1"
+                              "-isystem/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include"
+                              "-isystem/usr/local/include"
+                              "-isystem/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/clang/11.0.0/include"
+                              "-isystem/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/include"
+                              "-isystem/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/include"
+                              "-isystem/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/System/Library/Frameworks"]
+                  :resourceDir "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/clang/11.0.0")))
+  )
+
+;; (after! ccls
+;;   (setq ccls-initialization-options
+;;         '(:clang (:extraArgs ["/Library/Developer/CommandLineTools/usr/include/c++/v1", "/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include"])
+;;                  (:resourceDir "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/clang/11.0.0/include")))
+;;   )
+
 ;;; CC0 compiler
 (setq c0-root "~/dev/cmu15-122/cc0/")
 (load (concat c0-root "c0-mode/c0.el"))
@@ -55,8 +87,7 @@
     (indent-tabs-mode . nil)))
 
 (add-hook 'protobuf-mode-hook
-  (lambda () (c-add-style "protobuf-style" my-protobuf-style t)))
-(add-hook 'protobuf-mode-hook 'display-line-numbers-mode)
+          (lambda () (c-add-style "protobuf-style" my-protobuf-style t)))
 
 ;;; Java
 ;; JAVA_HOME decides the java sdk version
